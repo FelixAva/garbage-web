e8b931520b71482fb958febe9d0b534b9d8b7b8e
Felix <felavalos@gmail.com>
2025-04-07 17:13:03 +0900

    Garbage-web page creation (React-vite)

---
+++ src/api/firebase-manager.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_CFre7z2Tpv2Zu42Hn9Anf4s1Q5bu3X0",
+  authDomain: "garbage-classification-6c1b7.firebaseapp.com",
+  projectId: "garbage-classification-6c1b7",
+  storageBucket: "garbage-classification-6c1b7.firebasestorage.app",
+  messagingSenderId: "1047470141321",
+  appId: "1:1047470141321:web:c06e4257343ca83a71fb58",
+  measurementId: "G-WMGPLMT4PE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
