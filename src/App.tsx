da8e24a8e2c2934f1f091f019501c9848d46a212
Felix <felavalos@gmail.com>
2025-04-15 15:36:34 +0900

    Create render Controller's card

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
-import usePositions from '@/hooks/usePositions';
 import useControllers from '@/hooks/useControllers';
-import { Controller, Position } from '@/interfaces/firebase.interface';
+import ControllerCard from '@/components/ControllerCard';
+import { Controller } from '@/interfaces/firebase.interface';
 
 function App() {
   const [isActive, setIsActive] = useState<boolean>(false);
 
-  const {
-    positions,
-    positionsSubscription
-  } = usePositions();
+  // const {
+  //   positions,
+  //   positionsSubscription
+  // } = usePositions();
 
   const {
     controllers,
@@ -17,7 +17,7 @@ function App() {
   } = useControllers();
 
   useEffect(() => {
-    positionsSubscription();
+    // positionsSubscription();
     controllersSubscription();
   }, []);
 
@@ -27,18 +27,16 @@ function App() {
 
   return (
     <>
-    {
+    {/* {
       positions?.map(( ctrl: Position, indx: number ) => (
         <h1 key={indx} style={{fontSize: 20, color: 'black'}}>
           {ctrl.id}
         </h1>
       ))
-    }
+    } */}
     {
       controllers?.map(( ctrl: Controller, indx: number ) => (
-        <h1 key={indx} style={{fontSize: 20, color: 'black'}}>
-          {ctrl.id}
-        </h1>
+        <ControllerCard key={indx} {...ctrl}/>
       ))
     }
       <button onClick={ () => onPress() }> On/Off </button>
---
+++ src/components/ControllerCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Controller } from "@/interfaces/firebase.interface";
+
+const ControllerCard = ({ id, isActive,name }: Controller) => {
+
+  return (
+    <div style={{borderWidth: 1, borderColor: 'black', borderRadius: 10}}>
+      
+    </div>
+  );
+};
+
+export default ControllerCard;
